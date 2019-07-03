d8d2c5bf523f3120e5fab21fab0bc1ee737eed38
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-07-03 13:04:56 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "azure_ad_object_id_server" {
-  value = "${k8s_azuread_application.server.id}"
+  value = "$azuread_application.server.id}"
 }
 
 output "azure_ad_object_id_client" {
-  value = "${k8s_azuread_application.client.id}"
+  value = "${azuread_application.client.id}"
 }
