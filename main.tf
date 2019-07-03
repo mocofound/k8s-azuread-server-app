222bee9f052a888dcdacb54a0413a1a6191a59f8
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-07-03 12:59:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,37 @@
 
+resource "k8s_azuread_application" "server" {
+  name                       = "k8s azuread rbac server app"
+  homepage                   = "https://myaksserver"
+  identifier_uris            = ["https://myaksserver"]
+  reply_urls                 = ["https://replyurl"]
+  available_to_other_tenants = false
+  oauth2_allow_implicit_flow = true
+  type                       = "webapp/api"
+
+  required_resource_access {
+    resource_app_id = "00000003-0000-0000-c000-000000000000"
+
+    resource_access {
+      id = "7ab1d382-f21e-4acd-a863-ba3e13f7da61"
+      type = "Role"
+    }
+    resource_access {
+      id = ".e1fe6dd8-ba31-4d61-89e7-88639da4683d"
+      type = "Scope"
+    }
+
+    resource_access {
+      id = "06da0dbc-49e2-44d2-8312-53f166ab848a"
+      type = "Scope"
+    }
+  }
+
+  required_resource_access {
+    resource_app_id = "00000002-0000-0000-c000-000000000000"
+
+    resource_access {
+      id = "..."
+      type = "Scope"
+    }
+  }
+}
