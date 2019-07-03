4e4dd4d1ced07cbc1a025a460442fac68c1a6e86
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-07-03 18:33:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,16 +38,16 @@ resource "azuread_application" "server" {
     }
   }
    
-/*
+
   required_resource_access {
     resource_app_id = "00000002-0000-0000-c000-000000000000"
 
     resource_access {
-      id = "..."
+      id = "5778995a-e1bf-45b8-affa-663a9f3f4d04"
       type = "Scope"
     }
   }
-  */
+
 }
 /*
 oAuthPermissionId=$(az ad app show --id $serverApplicationId --query "oauth2Permissions[0].id" -o tsv)
